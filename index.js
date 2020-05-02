6994abd293f991fd733b604e33fe3005164d6e0d
Rick Martens <info@rickmartens.nl>
2020-05-02 19:16:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const botconfig = require("./botconfig.json");
+const discord   = require("discord.js");
+const client    = new discord.Client({disableMentions: "everyone"});
+
+client.on("ready", async () => {
+    console.log(`${client.user.username} is now running correctly!`);
+});
